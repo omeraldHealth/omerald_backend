82ebe6a8e087376d2ce437db88538b5c1a7e4d69
msaif001 <saifmohammed888@gmail.com>
2024-03-16 15:38:17 +0530

    fix: diag setting

--- src/api/v1/models/diagnosticSettings.js
+++ src/api/v1/models/diagnosticSettings.js
@@ -1,13 +1,18 @@
-const mongoose  = require("mongoose")
+const mongoose = require("mongoose");
 
-const diagnosticSettings = new mongoose.Schema({
-  key: { type: String, required: true },
-  value: mongoose.Mixed
-});
+const diagnosticSettingsSchema = new mongoose.Schema({
+  settings: {
+    FAQs: { type: String, required: true, default: "No Data" },
+    PrivacyPolicy: { type: String, required: true, default: "No Data" },
+    TermsOfService: { type: String, required: true, default: "No Data" },
+    PlatformConsent: { type: String, required: true, default: "No Data" },
+    Disclaimer: { type: String, required: true, default: "No Data" },
+    CustomerSupport: { type: String, required: true, default: "No Data" },
+  }
+}, { minimize: false }); // This option prevents MongoDB from automatically removing empty objects
 
 mongoose.models = {};
 
-const DiagnosticSettingsModel = mongoose.model('diagnosticSetting',diagnosticSettings);
+const DiagnosticSettingsModel = mongoose.model('DiagnosticSettings', diagnosticSettingsSchema);
 
-module.exports = DiagnosticSettingsModel
-
+module.exports = DiagnosticSettingsModel;
--- src/api/v1/models/userSettings.js
+++ src/api/v1/models/userSettings.js
@@ -1,13 +1,17 @@
-const mongoose  = require("mongoose")
+const mongoose = require("mongoose");
 
-const userSettings = new mongoose.Schema({
-  key: { type: String, required: true, unique: true },
-  value: mongoose.Mixed,
-  isActive: { type: Boolean, default: true },
-});
+const userSettingsSchema = new mongoose.Schema({
+  settings: {
+    FAQs: { type: String,  description: "Frequently Asked Questions section" , default: "No Data" },
+    PrivacyPolicy: { type: String,  description: "Privacy Policy section" , default: "No Data" },
+    TermsOfService: { type: String,  description: "Terms of Service section" , default: "No Data" },
+    PlatformConsent: { type: String,  description: "Platform Consent section" , default: "No Data" },
+    Disclaimer: { type: String,  description: "Disclaimer section" , default: "No Data" },
+    CustomerSupport: { type: String,  description: "Customer Support section",  default: "No Data" }
+  } }, { minimize: false }); // { minimize: false } prevents empty objects from being omitted
 
 mongoose.models = {};
 
-const UserSettingsModel = mongoose.model('userSettings', userSettings);
+const UserSettingsModel = mongoose.model('UserSettings', userSettingsSchema);
 
-module.exports = UserSettingsModel
+module.exports = UserSettingsModel;
