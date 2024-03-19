e649d68f25deee8d7e2c47ee430546f9d5073a33
msaif001 <saifmohammed888@gmail.com>
2024-03-19 22:44:39 +0530

    fix: dev

--- src/config/serverSetup.js
+++ src/config/serverSetup.js
@@ -9,40 +9,57 @@ const { authenticateToken } = require('../utils/middleware');
 function setupServer(port) {
   const app = express();
 
-  // CORS setup
   const corsOptions = {
-    origin: '*',
+    origin: "*",
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    allowedHeaders: '*',
     credentials: true,
-    optionSuccessStatus: 200
+    optionsSuccessStatus: 200, // For legacy browsers
+    maxAge: 86400 // 24 hours
   };
   app.use(cors(corsOptions));
+
   app.options('*', cors(corsOptions));
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
 
   // Middleware setup
   app.use(express.json());
   app.use(compression());
- // Public route
- app.post('/api/v1/auth/getAuthToken', authenticateAPIUser);
 
- // Apply JWT authentication to all subsequent routes
- app.use(authenticateToken);
- app.use(routeUsage);
-
-  // Set CORS headers
-  app.use((req, res, next) => {
-    res.setHeader('Referrer-Policy', 'no-referrer');
-    res.setHeader('Content-Security-Policy', 'default-src *');
-    next();
+  // Example route
+  app.get('/', (req, res) => {
+    res.send('Hello, World!');
   });
+
+
+  
+  // Public route
+  app.post('/api/v1/auth/getAuthToken', authenticateAPIUser);
+
+  // Apply JWT authentication to all subsequent routes
+  app.use(authenticateToken);
+  
+  // Route usage tracking
+  app.use(routeUsage);
+
+  // It's best practice to define your security headers within the cors options or specifically for routes as needed.
+  // Removing the manual CORS header settings here to avoid conflict with the cors middleware
+
+  // MongoDB connection setup
+  connectToMongoDB(process.env.MONGODB_URI);
 
   // Start the server
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
-
-  // Mongoose connection setup
-  const mongodbURI = process.env.MONGODB_URI;
-  connectToMongoDB(mongodbURI);
 
   return app;
 }
