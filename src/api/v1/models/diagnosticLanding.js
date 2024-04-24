0ac6eabaed476d066042d2870d23e8f3fc131775
msaif001 <saifmohammed888@gmail.com>
2024-04-25 02:49:07 +0530

    fix: Landing Setting

---
+++ src/api/v1/models/diagnosticLanding.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+
+const diagnosticLandingSchema = new mongoose.Schema({
+  landing: {
+    customerLogos: [{
+      url: {
+        type: String,
+        required: [true, 'URL is required'],
+        trim: true
+      },
+      title: {
+        type: String,
+        required: [true, 'Title is required'],
+        trim: true
+      },
+      meta: {
+        type: String,
+        trim: true
+      }
+    }],
+    advertisementBanners: [{
+      url: {
+        type: String,
+        required: [true, 'URL is required'],
+        trim: true
+      },
+      title: {
+        type: String,
+        required: [true, 'Title is required'],
+        trim: true
+      },
+      meta: {
+        type: String,
+        required: [true, 'Meta is required'],
+        trim: true
+      },
+      getStartedUrl: {
+        type: String,
+        trim: true
+      },
+      demoVideoUrl: {
+        type: String,
+        trim: true
+      }
+    }],
+    testimonials: [{
+      name: {
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true
+      },
+      rating: {
+        type: Number,
+        required: [true, 'Rating is required'],
+        min: 1,
+        max: 5
+      },
+      comment: {
+        type: String,
+        trim: true
+      },
+      reviewDate: {
+        type: Date,
+        default: Date.now
+      }
+    }]
+  }
+}, {
+  timestamps: true, // Add created and updated timestamps
+});
+
+// Export the model directly from the schema definition
+module.exports = mongoose.model('DiagnosticLandingSettings', diagnosticLandingSchema);
